d5b85286a176ce1ac81453ed7b61c12599cb1cf5
Jonathan Ikpen <jonathanikpen4@gmail.com>
2024-01-18 01:03:34 +0100

    feat: add download

--- BIRTHGISTER/certificate/certificate.js
+++ BIRTHGISTER/certificate/certificate.js
@@ -13,39 +13,60 @@ const place = document.getElementById('place')
 const currentDay = document.getElementById('currentDay')
 const current_month_year = document.getElementById('current_month_year')
 
-fetch(`https://birthgister.vercel.app/api/users/${getCertId}`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+generateCertificate()
+function generateCertificate () {
+    const loadingOverlay = document.getElementById('loadingOverlay');
+    loadingOverlay.style.display = 'flex';
+
+    const animation = setupCertificateLoadingAnimation();
+
+    setTimeout(async () => {
+        try {
+
+            fetch(`https://birthgister.vercel.app/api/users/${getCertId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(async (data) => {
+                    console.log('Data retrieved successfully:', data);
+
+                    registrarName.value = 'Ahweyevu Bridget'
+                    town_state.value = data.user.place + ' / ' + data.user.town
+                    name.value = data.user.name
+                    birthDay.value = formatDate(data.user.dateofbirth).inputFormattedDate.day
+                    birth_month_year.value = formatDate(data.user.dateofbirth).inputFormattedDate.month_year
+                    town.value = data.user.town
+                    parentName.value = data.user.father?.fathersName ?? data.user.mother?.mothersName ?? data.user.parent?.parentName
+                    place.value = data.user.place
+                    currentDay.value = formatDate(data.user.dateofbirth).todayFormattedDate.day
+                    current_month_year.value = formatDate(data.user.dateofbirth).todayFormattedDate.month_year
+
+                    downloadCertificate()
+                })
+                .catch(error => {
+                    console.error('Error retrieving data:', error);
+                    alert('Error creating certificate. Please try again.');
+                })
+                .finally(() => {
+                loadingOverlay.style.display = 'none';
+                animation.destroy();
+            })
+
+        } catch(e) {
+            alert(e.message)
+            loadingOverlay.style.display = 'none';
+            animation.destroy();
         }
-        return response.json();
-    })
-    .then(async (data) => {
-        console.log('Data retrieved successfully:', data);
-
-        registrarName.value = 'Ahweyevu Bridget'
-        town_state.value = data.user.place + ' / ' + data.user.town
-        name.value = data.user.name
-        birthDay.value = formatDate(data.user.dateofbirth).inputFormattedDate.day
-        birth_month_year.value = formatDate(data.user.dateofbirth).inputFormattedDate.month_year
-        town.value = data.user.town
-        parentName.value = data.user.father?.fathersName ?? data.user.mother?.mothersName ?? data.user.parent?.parentName
-        place.value = data.user.place
-        currentDay.value = formatDate(data.user.dateofbirth).todayFormattedDate.day
-        current_month_year.value = formatDate(data.user.dateofbirth).todayFormattedDate.month_year
-
-    })
-    .catch(error => {
-        console.error('Error retrieving data:', error);
-        alert('Error creating certificate. Please try again.');
-    });
-
-
+    }, 2000);
+}
 
 function formatDate(inputDate) {
     // Parse the input date string
@@ -103,5 +124,31 @@ function addDaySuffix(day) {
                 return `${day}th`;
         }
     }
+}
+
+
+function downloadCertificate() {
+    const element = document.getElementById('certificate');
+    html2pdf(element, {
+        margin: 5,
+        filename: 'certificate.pdf',
+        image: { type: 'png', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a5', orientation: 'portrait' }
+    }).then((pdf) => {
+        pdf.save();
+    });
+}
+
+
+function setupCertificateLoadingAnimation() {
+    const lottieContainer = document.getElementById('lottieContainer');
+    return bodymovin.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '../loading.json',
+    });
 }
 
--- BIRTHGISTER/child_script.js
+++ BIRTHGISTER/child_script.js
@@ -1,175 +1,176 @@
-
-document.getElementById('hamburger').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('menu').classList.toggle('active');
+document.getElementById("hamburger").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.getElementById("menu").classList.toggle("active");
 });
 
 const fields = [
-    "name",
-    "dateofbirth",
-    "age",
-    "gender",
-    "place",
-    "town",
-    "mothersName",
-    "mothersResidence",
-    "mothersAgeAtBirth",
-    "mothersMaritalStatus",
-    "mothersNationality",
-    "mothersStateOfOrigin",
-    "mothersOccupation",
-    "mothersPhoneNumber",
-    "mothersNationalID",
-    "fathersName",
-    "fathersResidence",
-    "fathersCurrentAge",
-    "fathersNationality",
-    "fathersStateOfOrigin",
-    "fathersOccupation",
-    "fathersPhoneNumber",
-    "fathersNationalID",
-    "informantName",
-    "informantRelationshipToChild",
-    "informantResidence",
-    "informantNationalID",
-    "informantPhoneNumber",
+  "name",
+  "dateofbirth",
+  "age",
+  "gender",
+  "place",
+  "town",
+  "mothersName",
+  "mothersResidence",
+  "mothersAgeAtBirth",
+  "mothersMaritalStatus",
+  "mothersNationality",
+  "mothersStateOfOrigin",
+  "mothersOccupation",
+  "mothersPhoneNumber",
+  "mothersNationalID",
+  "fathersName",
+  "fathersResidence",
+  "fathersCurrentAge",
+  "fathersNationality",
+  "fathersStateOfOrigin",
+  "fathersOccupation",
+  "fathersPhoneNumber",
+  "fathersNationalID",
+  "informantName",
+  "informantRelationshipToChild",
+  "informantResidence",
+  "informantNationalID",
+  "informantPhoneNumber",
 ];
 function validateChildForm(e) {
-    e.preventDefault()
-    let isValid = true;
+  e.preventDefault();
+  let isValid = true;
 
-    function checkEmptyFields() {
-        for (let i = 0; i < fields.length; i++) {
-          const fieldValue = document.getElementById(fields[i]).value;
-          if (fieldValue === "") {
-            return false;
-          }
-        }
-        return true;
+  function checkEmptyFields() {
+    for (let i = 0; i < fields.length; i++) {
+      const fieldValue = document.getElementById(fields[i]).value;
+      if (fieldValue === "") {
+        return false;
       }
-
-      if (!checkEmptyFields()) {
-        alert("Please complete all details before submitting.");
-        isValid = false;
-      }
-
-      const childAge = document.getElementById("age").value;
-      if (isNaN(childAge)) {
-        alert("Please enter a valid number for the child's age.");
-        isValid = false;
-      }
-
-
-      const gender = document.querySelector('input[name="gender"]:checked');
-      if (!gender) {
-        alert("Please select a gender before submitting.");
-        isValid = false;
-      }
-
-      if(isValid) {
-          submitChildData(e)
-      }
-
-      return isValid;
     }
+    return true;
+  }
 
+  if (!checkEmptyFields()) {
+    alert("Please complete all details before submitting.");
+    isValid = false;
+  }
 
-async function submitChildData(e) {
-    e.preventDefault()
+  const childAge = document.getElementById("age").value;
+  if (isNaN(childAge)) {
+    alert("Please enter a valid number for the child's age.");
+    isValid = false;
+  }
 
-    const file = document.getElementById('photo').files[0]
-    if(!file) {
-        return alert('upload photo')
-    }
-    const photo = await fileToBase64(file)
+  const gender = document.querySelector('input[name="gender"]:checked');
+  if (!gender) {
+    alert("Please select a gender before submitting.");
+    isValid = false;
+  }
 
-    const loadingOverlay = document.getElementById('loadingOverlay');
-    loadingOverlay.style.display = 'flex';
+  if (isValid) {
+    submitChildData(e);
+  }
 
-    const animation = setupChildLoadingAnimation();
-
-    // Simulate API request delay
-    setTimeout(async () => {
-        try {
-            const data = fields.map(fieldName => {
-                const fieldValue = document.getElementById(fieldName).value;
-                return { [fieldName]: fieldValue };
-            });
-
-            const combinedData = Object.assign({ child: true, photo: photo }, ...data);
-            console.log('Combined Data:', combinedData);
-
-            fetch('https://birthgister.vercel.app/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(combinedData),
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(async (data) => {
-                    console.log('Data submitted successfully:', data);
-                    const id = await data.data.id
-                    console.log(`/certificate?id=${id}`)
-
-                    if(await id) {
-                        window.location.href = `/certificate/?id=${id}`
-                    }
-                })
-                .catch(error => {
-                    console.error('Error submitting data:', error);
-                    alert('Error submitting data. Please try again.');
-                }).finally(() => {
-                    loadingOverlay.style.display = 'none';
-                    animation.destroy();
-                })
-
-        } catch(e) {
-            alert(e.message)
-            loadingOverlay.style.display = 'none';
-            animation.destroy();
-        }
-    }, 2000);
+  return isValid;
 }
 
+async function submitChildData(e) {
+  e.preventDefault();
+
+  const file = document.getElementById("photo").files[0];
+  if (!file) {
+    return alert("upload photo");
+  }
+  const photo = await fileToBase64(file);
+
+  const loadingOverlay = document.getElementById("loadingOverlay");
+  loadingOverlay.style.display = "flex";
+
+  const animation = setupChildLoadingAnimation();
+
+  // Simulate API request delay
+  setTimeout(async () => {
+    try {
+      const data = fields.map((fieldName) => {
+        const fieldValue = document.getElementById(fieldName).value;
+        return { [fieldName]: fieldValue };
+      });
+
+      const combinedData = Object.assign(
+        { child: true, photo: photo },
+        ...data
+      );
+      console.log("Combined Data:", combinedData);
+
+      fetch("https://birthgister.vercel.app/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(combinedData),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(async (data) => {
+          console.log("Data submitted successfully:", data);
+          const id = await data.data.id;
+          console.log(`/certificate?id=${id}`);
+
+          if (await id) {
+            window.location.href = `/certificate/?id=${id}`;
+          }
+        })
+        .catch((error) => {
+          console.error("Error submitting data:", error);
+          alert("Error submitting data. Please try again.");
+        })
+        .finally(() => {
+          loadingOverlay.style.display = "none";
+          animation.destroy();
+        });
+    } catch (e) {
+      alert(e.message);
+      loadingOverlay.style.display = "none";
+      animation.destroy();
+    }
+  }, 2000);
+}
 
 function fileToBase64(file) {
-    return new Promise((resolve, reject) => {
-        const maxSizeInBytes = 1024 * 1024; // 1MB
+  return new Promise((resolve, reject) => {
+    const maxSizeInBytes = 1024 * 1024; // 1MB
 
-        // Check file size before attempting to read
-        if (file.size > maxSizeInBytes) {
-            const error = new Error('File size exceeds the limit (1MB). Please choose a smaller file.');
-            reject(error);
-            return;
-        }
+    // Check file size before attempting to read
+    if (file.size > maxSizeInBytes) {
+      const error = new Error(
+        "File size exceeds the limit (1MB). Please choose a smaller file."
+      );
+      reject(error);
+      return;
+    }
 
-        const reader = new FileReader();
+    const reader = new FileReader();
 
-        reader.onload = function (event) {
-            resolve(event.target.result.split(',')[1]); // Extract the Base64-encoded part
-        };
+    reader.onload = function (event) {
+      resolve(event.target.result.split(",")[1]); // Extract the Base64-encoded part
+    };
 
-        reader.onerror = function (error) {
-            reject(error);
-        };
+    reader.onerror = function (error) {
+      reject(error);
+    };
 
-        reader.readAsDataURL(file);
-    });
+    reader.readAsDataURL(file);
+  });
 }
 
 function setupChildLoadingAnimation() {
-    const lottieContainer = document.getElementById('lottieContainer');
-    return bodymovin.loadAnimation({
-        container: lottieContainer,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'loading.json',
-    });
+  const lottieContainer = document.getElementById("lottieContainer");
+  return bodymovin.loadAnimation({
+    container: lottieContainer,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "loading.json",
+  });
 }
